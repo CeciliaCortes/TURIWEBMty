a66e8cc912e7c02eb2cc849dc2a20f6799b36d7f
USUARIO <USUARIO@CECILIACORTES>
2016-11-28 14:41:23 -0600

    login

--- TURIWEBMty/TURIWEBMty/Controllers/HomeController.cs
+++ TURIWEBMty/TURIWEBMty/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TURIWEBMty.Models;
 
 namespace TURIWEBMty.Controllers
 {
@@ -10,9 +11,32 @@ namespace TURIWEBMty.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
-            return View();
+            logio obj = new logio();
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult Index(logio objuserlogin)
+        {
+            var display = Userloginvalues().Where(m => m.Correo == objuserlogin.Correo && m.Contraseña == objuserlogin.Contraseña).FirstOrDefault();
+            if (display != null)
+            {
+                ViewBag.Status = "CORRECT UserNAme and Password";
+            }
+            else
+            {
+                ViewBag.Status = "INCORRECT UserName or Password";
+            }
+            return View(objuserlogin);
+        }
+        public List<logio> Userloginvalues()
+        {
+            List<logio> objModel = new List<logio>();
+            objModel.Add(new logio { Correo = "user1", Contraseña = "Password1" });
+            objModel.Add(new logio { Correo = "user2", Contraseña = "Password2" });
+            objModel.Add(new logio { Correo = "user3", Contraseña = "Password3" });
+            objModel.Add(new logio { Correo = "user4", Contraseña = "Password4" });
+            objModel.Add(new logio { Correo = "user5", Contraseña = "Password5" });
+            return objModel;
         }
     }
 }

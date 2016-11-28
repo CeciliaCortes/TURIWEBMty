e7d4fcd7833b279820dcfd68de3587973f4ce2aa
sandra janeth rodriguez perez <yanet.rdz.95@gmail.com>
2016-11-27 20:41:52 -0600

    cree carpeta

--- TURIWEBMty/TURIWEBMty/Content/TuriContext.cs
+++ TURIWEBMty/TURIWEBMty/Content/TuriContext.cs
@@ -19,6 +19,7 @@ namespace TURIWEBMty.Content
         public DbSet<Transporte> Transportes { get; set; }
         public DbSet<Lugar> Lugares { get; set; }
         public DbSet<Ubicacion> Ubicaciones { get; set; }
+        public DbSet<logio> logio { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
---
+++ TURIWEBMty/TURIWEBMty/Models/logio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TURIWEBMty.Models
+{
+    public class logio
+    {
+
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+    }
+}

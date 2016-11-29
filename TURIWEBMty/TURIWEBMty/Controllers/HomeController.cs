9cd3c32d29ff83e2e30d74ba783d8f975f959147
USUARIO <USUARIO@CECILIACORTES>
2016-11-29 08:23:27 -0600

    ..

--- TURIWEBMty/TURIWEBMty/Controllers/HomeController.cs
+++ TURIWEBMty/TURIWEBMty/Controllers/HomeController.cs
@@ -14,29 +14,5 @@ namespace TURIWEBMty.Controllers
             logio obj = new logio();
             return View(obj);
         }
-        [HttpPost]
-        public ActionResult Index(logio objuserlogin)
-        {
-            var display = Userloginvalues().Where(m => m.Correo == objuserlogin.Correo && m.Contraseña == objuserlogin.Contraseña).FirstOrDefault();
-            if (display != null)
-            {
-                ViewBag.Status = "Bienvenido";
-            }
-            else
-            {
-                ViewBag.Status = "Contraseña o Usuario Incorrecto";
-            }
-            return View(objuserlogin);
-        }
-        public List<logio> Userloginvalues()
-        {
-            List<logio> objModel = new List<logio>();
-            objModel.Add(new logio { Correo = "user1", Contraseña = "Password1" });
-            objModel.Add(new logio { Correo = "user2", Contraseña = "Password2" });
-            objModel.Add(new logio { Correo = "user3", Contraseña = "Password3" });
-            objModel.Add(new logio { Correo = "user4", Contraseña = "Password4" });
-            objModel.Add(new logio { Correo = "user5", Contraseña = "Password5" });
-            return objModel;
-        }
     }
 }
---
+++ TURIWEBMty/TURIWEBMty/Controllers/logiosController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TURIWEBMty.Content;
+using TURIWEBMty.Models;
+
+namespace TURIWEBMty.Controllers
+{
+    public class logiosController : ApiController
+    {
+        private TuriContext db = new TuriContext();
+
+        // GET: api/logios
+        public IQueryable<logio> Getlogin()
+        {
+            return db.login;
+        }
+
+        // GET: api/logios/5
+        [ResponseType(typeof(logio))]
+        public IHttpActionResult Getlogio(int id)
+        {
+            logio logio = db.login.Find(id);
+            if (logio == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(logio);
+        }
+
+        // PUT: api/logios/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putlogio(int id, logio logio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != logio.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(logio).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!logioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/logios
+        [ResponseType(typeof(logio))]
+        public IHttpActionResult Postlogio(logio logio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.login.Add(logio);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = logio.ID }, logio);
+        }
+
+        // DELETE: api/logios/5
+        [ResponseType(typeof(logio))]
+        public IHttpActionResult Deletelogio(int id)
+        {
+            logio logio = db.login.Find(id);
+            if (logio == null)
+            {
+                return NotFound();
+            }
+
+            db.login.Remove(logio);
+            db.SaveChanges();
+
+            return Ok(logio);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool logioExists(int id)
+        {
+            return db.login.Count(e => e.ID == id) > 0;
+        }
+    }
+}
